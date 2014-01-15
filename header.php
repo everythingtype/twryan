d107d1246c9273a8fcd1b27ed326afa65ae9b654
Marty Spellerberg <marty@martyspellerberg.com>
2014-01-14 19:53:14 -0600

    Favicons

--- header.php
+++ header.php
@@ -18,6 +18,15 @@
 	
 		<meta name="author" content="http://www.everything-type-company.com, http://martyspellerberg.com" />
 
+		<!-- Fav Icons: Browser, iOS, Windows 8 -->
+		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon.ico">
+		<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon-114.png" />
+		<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon-72.png" />
+		<link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon-57.png" />
+		<meta name="application-name" content="<?php bloginfo( 'name' ); ?>"/> 
+		<meta name="msapplication-TileColor" content="#ffffff"/> 
+		<meta name="msapplication-TileImage" content="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon-144.png"/>
+
 		<?php wp_head(); ?>
 
 </head>
